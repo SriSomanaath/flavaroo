93faf0bf4f744cee31d392053fc8cd60306b9cf3
SriSomanaath <govindalapudisrinath@gmail.com>
2023-11-16 11:55:53 +0530

    LogIn component

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,23 @@
-import { React } from 'react'
+import { React } from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import LogIn from "./pages/LogIn"
 
 function App() {
 
   return (
     <>
-    <h1>Hello React!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/logIn" element={<LogIn />} />
+        </Routes>
+        <Link to="/logIn">
+          <button className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+            Log In
+          </button>
+        </Link>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
---
+++ vite-project/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import { React } from 'react'
+
+const LogIn = () => {
+
+  return (
+    <>
+    <h1>Vachesanu Ra bulloda</h1>
+    </>
+  )
+}
+
+export default LogIn
